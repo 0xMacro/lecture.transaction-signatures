d56441ac2ef44ad4e45c7c9594749ba88376c940
Thomas Taylor <twtaylor7@gmail.com>
2022-06-13 08:37:54 -0700

    Initial commit of tx signatures lecture

---
+++ contracts/Signer.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract Signer {
+    using ECDSA for bytes32;
+
+
+
+
+    // stored nonces
+    mapping(address => uint256) nonces;
+
+
+
+
+
+    // helper for verify() - uses OZ library above
+    function recoverSigner(bytes32 data, bytes memory signature) public pure returns (address) {
+        return data.recover(signature);
+    }
+
+
+
+
+
+    // helper for verify()
+    function getMessageHash(address _to, uint256 _amount, string memory _message, uint256 nonce) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_to, _amount, _message, nonce));
+    }
+
+
+
+
+
+    // helper for verify()
+    function getEthSignedMessageHash(bytes32 _messageHash) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash));
+    }
+
+
+
+
+    function verify(address _signer, address _to, uint256 _amount, string memory _message, uint256 nonce, bytes memory signature) public returns (bool) {
+        bytes32 messageHash = getMessageHash(_to, _amount, _message, nonce);
+
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+
+        address actualSigner = recoverSigner(ethSignedMessageHash, signature);
+
+        require(nonces[actualSigner] == nonce - 1, "VerifySig fail");
+        nonces[actualSigner] = nonce;
+
+        return actualSigner == _signer;
+    }
+
+
+
+
+
+
+    function sendEthWithValidSig(address signer, address to, uint256 amount, string memory message, uint256 nonce, bytes memory signature) public {
+        require(verify(signer, to, amount, message, nonce, signature), "InvalidSig");
+
+        (bool success, ) = to.call{value: amount}("");
+        require(success, "call failure");
+    }
+}
